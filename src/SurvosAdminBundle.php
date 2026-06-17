201dc831ce22ba40122a0cb0238e3dbffa6cd429
tacman <tacman@users.noreply.github.com>
2026-06-17 12:11:10 +0000

    better kit-bundle integration

--- src/SurvosAdminBundle.php
+++ src/SurvosAdminBundle.php
@@ -13,6 +13,7 @@ use Symfony\Component\DependencyInjection\Argument\TaggedIteratorArgument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+// Symfony\Component\HttpKernel\Bundle\Bundle <-- Flex auto-registration marker (see Survos\Kit\AbstractSurvosBundle)
 final class SurvosAdminBundle extends AbstractSurvosBundle
 {
     use HasConfigurableRoutes;
